54bf45c5503c5553682da644f512707169129bb3
Choris98989 <chorisss814@gmail.com>
2024-02-29 11:13:12 +0500

    add dars-1.py

---
+++ dars-1.py
@@ -0,0 +1,21 @@
+# if 5 > 3:
+#     print("5 kotta 3 dan")
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
